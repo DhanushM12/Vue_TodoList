94df554ba4a0c41085d16bc3e90c2c2e476a854e
DhanushM12 <dhanushmukhi@gmail.com>
2021-01-11 01:22:13 +0530

    added the AddTodo component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <Header />
+    <AddTodo />
     <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
   </div>
 </template>
 
 <script>
 import Header from './components/layout/Header';
+import AddTodo from './components/AddTodo';
 import Todos from './components/Todos';
 export default {
   name: 'App',
   components: {
     Header,
+    AddTodo,
     Todos
   },
   data() {
@@ -52,5 +55,16 @@ export default {
   body {
     font-family: Arial, Helvetica, sans-serif;
     line-height: 1.4;
+  }
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+  .btn:hover {
+    background: #666;
   }
 </style>
---
+++ src/components/AddTodo.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+    <form>
+      <input type="text" v-model="title" name="title" placeholder="Add Tasks...">
+      <input type="submit" value="Submit" class="btn">
+    </form>
+  </div>
+</template>
+<script>
+export default {
+    name: "AddTodo"
+}
+</script>
+<style scoped>
+form {
+    display: flex;
+  }
+  input[type="text"] {
+    flex: 10;
+    padding: 5px;
+  }
+  input[type="submit"] {
+    flex: 2;
+  }
+</style>
